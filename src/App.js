af164498cf43ade0a3964e66d46c41a733e6ad66
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-02-20 04:20:50 +0530

    infoBox added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { FormControl, Select, MenuItem } from '@material-ui/core';
+import InfoBox from './components/infoBox';
 import './App.css';
 
 function App() {
 	const [countries, setCountries] = useState([]);
+	const [country, setCountry] = useState('Worldwide');
 
 	const API_ENDPOINT = 'https://disease.sh/v3/covid-19/countries';
 
@@ -19,7 +21,14 @@ function App() {
 					setCountries(countries);
 				});
 		};
+		getCountriesData();
 	});
+
+	const onCountryChange = (e) => {
+    e.preventDefault();
+    const countryCode = e.target.value;
+		setCountry(countryCode);
+	};
 
 	return (
 		<div className='App'>
@@ -27,12 +36,21 @@ function App() {
 				<h1>Covid 19 tracker</h1>
 
 				<FormControl className='App_dropdown'>
-					<Select labelId='label' id='select' value='20'>
-						<MenuItem value='10'>Ten</MenuItem>
-						<MenuItem value='20'>Twenty</MenuItem>
+					<Select variant='outlined' value={country} onChange={onCountryChange}>
+						<MenuItem value='Worldwide'>Worldwide</MenuItem>
+						{countries.map((country) => (
+							<MenuItem value={country.value}>{country.name}</MenuItem>
+						))}
 					</Select>
 				</FormControl>
-			</div>
+      </div>
+      
+      <div className='App_stats'>
+        <InfoBox title='CoronaVirus Cases' total={1000} cases={1}/>
+        <InfoBox title='Recoveries'  total={1000} cases={1} />
+        <InfoBox title= 'Deaths'  total={1000} cases={1} />
+      </div>
+      
 		</div>
 	);
 }
---
+++ src/components/infoBox.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core';
+
+function infoBox({title,total, cases}) {
+    return (
+        <Card>
+            <CardContent> 
+               
+                <Typography className='infoBox_title' color="textSecondary">
+                    {title}
+                </Typography>
+              
+                <h3 className='infoBox_cases'>
+                 {cases}
+                </h3>
+              
+                <Typography className='infoBox_total'>
+                 total:{total}
+                </Typography>
+            </CardContent>
+            
+        </Card>
+    )
+}
+
+export default infoBox;
