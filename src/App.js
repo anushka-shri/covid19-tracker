4f922813d7f37241cee45c3990719eb12cac1cc4
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-02-20 03:13:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { FormControl, Select, MenuItem } from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [countries, setCountries] = useState([]);
+
+	const API_ENDPOINT = 'https://disease.sh/v3/covid-19/countries';
+
+	useEffect(() => {
+		const getCountriesData = async () => {
+			await fetch(API_ENDPOINT)
+				.then((response) => response.json())
+				.then((data) => {
+					const countries = data.map((country) => ({
+						name: country.country,
+						value: country.countryInfo.iso2,
+					}));
+					setCountries(countries);
+				});
+		};
+	});
+
+	return (
+		<div className='App'>
+			<div className='App_header'>
+				<h1>Covid 19 tracker</h1>
+
+				<FormControl className='App_dropdown'>
+					<Select labelId='label' id='select' value='20'>
+						<MenuItem value='10'>Ten</MenuItem>
+						<MenuItem value='20'>Twenty</MenuItem>
+					</Select>
+				</FormControl>
+			</div>
+		</div>
+	);
 }
 
 export default App;
