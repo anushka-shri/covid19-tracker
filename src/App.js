c2111171d0b3e5fc8fd0cf90bdc865dcd5a16fd5
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-02-20 05:08:12 +0530

    Map component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { FormControl, Select, MenuItem } from '@material-ui/core';
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  Card,
+  CardContent
+} from '@material-ui/core';
 import InfoBox from './components/infoBox';
+import Map from './components/Map';
 import './App.css';
 
 function App() {
@@ -25,32 +32,45 @@ function App() {
 	});
 
 	const onCountryChange = (e) => {
-    e.preventDefault();
-    const countryCode = e.target.value;
+		e.preventDefault();
+		const countryCode = e.target.value;
 		setCountry(countryCode);
 	};
 
 	return (
 		<div className='App'>
-			<div className='App_header'>
-				<h1>Covid 19 tracker</h1>
+			<div className='App_left'>
+				<div className='App_header'>
+					<h1>Covid 19 tracker</h1>
 
-				<FormControl className='App_dropdown'>
-					<Select variant='outlined' value={country} onChange={onCountryChange}>
-						<MenuItem value='Worldwide'>Worldwide</MenuItem>
-						{countries.map((country) => (
-							<MenuItem value={country.value}>{country.name}</MenuItem>
-						))}
-					</Select>
-				</FormControl>
-      </div>
-      
-      <div className='App_stats'>
-        <InfoBox title='CoronaVirus Cases' total={1000} cases={1}/>
-        <InfoBox title='Recoveries'  total={1000} cases={1} />
-        <InfoBox title= 'Deaths'  total={1000} cases={1} />
-      </div>
-      
+					<FormControl className='App_dropdown'>
+						<Select
+							variant='outlined'
+							value={country}
+							onChange={onCountryChange}>
+							<MenuItem value='Worldwide'>Worldwide</MenuItem>
+							{countries.map((country) => (
+								<MenuItem value={country.value}>{country.name}</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</div>
+
+				<div className='App_stats'>
+					<InfoBox title='CoronaVirus Cases' total={1000} cases={1} />
+					<InfoBox title='Recoveries' total={1000} cases={1} />
+					<InfoBox title='Deaths' total={1000} cases={1} />
+				</div>
+
+				<Map />
+			</div>
+
+      <Card className='App_right'>
+        <CardContent>
+          <h2>Table</h2>
+          <h2>Graph</h2>
+        </CardContent>
+      </Card>
 		</div>
 	);
 }
--- src/components/infoBox.js
+++ src/components/infoBox.js
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { Card, CardContent, Typography } from '@material-ui/core';
 
-function infoBox({title,total, cases}) {
-    return (
-        <Card>
-            <CardContent> 
-               
-                <Typography className='infoBox_title' color="textSecondary">
-                    {title}
-                </Typography>
-              
+function infoBox({ title, total, cases }) {
+	return (
+		<Card>
+			<CardContent>
+				<Typography className='infoBox_title' color='textSecondary'>
+					{title}
+				</Typography>
+
                 <h3 className='infoBox_cases'>
-                 {cases}
+                    {cases}
                 </h3>
-              
+
                 <Typography className='infoBox_total'>
-                 total:{total}
+                    total:{total}
                 </Typography>
-            </CardContent>
-            
-        </Card>
-    )
+			</CardContent>
+		</Card>
+	);
 }
 
 export default infoBox;
