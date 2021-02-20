e5b5f6800826a62a620aed666e2bfa7c671c11f6
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-02-20 05:35:13 +0530

    url set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import {
-  FormControl,
-  Select,
-  MenuItem,
-  Card,
-  CardContent
+	FormControl,
+	Select,
+	MenuItem,
+	Card,
+	CardContent,
 } from '@material-ui/core';
 import InfoBox from './components/infoBox';
 import Map from './components/Map';
@@ -13,12 +13,22 @@ import './App.css';
 function App() {
 	const [countries, setCountries] = useState([]);
 	const [country, setCountry] = useState('Worldwide');
+	const [countryInfo, setCountryInfo] = useState({});
 
-	const API_ENDPOINT = 'https://disease.sh/v3/covid-19/countries';
+	// const API_ENDPOINT = 'https://disease.sh/v3/covid-19/countries';
+
+   useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
+
 
 	useEffect(() => {
 		const getCountriesData = async () => {
-			await fetch(API_ENDPOINT)
+			await fetch('https://disease.sh/v3/covid-19/countries')
 				.then((response) => response.json())
 				.then((data) => {
 					const countries = data.map((country) => ({
@@ -31,10 +41,22 @@ function App() {
 		getCountriesData();
 	});
 
-	const onCountryChange = (e) => {
+	const onCountryChange = async (e) => {
 		e.preventDefault();
 		const countryCode = e.target.value;
 		setCountry(countryCode);
+
+		const url =
+			countryCode === 'Worldwide'
+				? 'https://disease.sh/v3/covid-19/all'
+				: `https://disease.sh/v3/covid-19/${countryCode}`;
+
+		await fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				setCountry(countryCode);
+				setCountryInfo(data);
+			});
 	};
 
 	return (
@@ -65,12 +87,12 @@ function App() {
 				<Map />
 			</div>
 
-      <Card className='App_right'>
-        <CardContent>
-          <h2>Table</h2>
-          <h2>Graph</h2>
-        </CardContent>
-      </Card>
+			<Card className='App_right'>
+				<CardContent>
+					<h2>Table</h2>
+					<h2>Graph</h2>
+				</CardContent>
+			</Card>
 		</div>
 	);
 }
